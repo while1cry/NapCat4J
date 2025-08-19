d27042a8ad0e2785e165c90a439b8d452ff0eae0
while1cry <while1cry@163.com>
2025-08-19 12:41:13 +0800

    build: 将 Jackson 加入到api引用中，确保部分 JsonNode 字段可被正常使用

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,8 +21,8 @@ dependencies {
     api("org.jetbrains:annotations:26.0.2")
     api("org.greenrobot:eventbus-java:3.3.1")
     api("org.slf4j:slf4j-api:2.0.17")
+    api("com.fasterxml.jackson.core:jackson-databind:2.19.2")
     implementation("org.java-websocket:Java-WebSocket:1.6.0")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.19.2")
 
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
