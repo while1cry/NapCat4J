5ad3593044fd8e9d69093288adb2ca0f88334104
while1cry <while1cry@163.com>
2025-08-20 20:44:09 +0800

    build: 更新版本号并调整依赖传递关系，准备大量变动结构

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "me.while1cry"
-version = "0.1.0-SNAPSHOT"
+version = "0.2.0-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_21
 java.targetCompatibility = JavaVersion.VERSION_21
 description = "Java QQ client based on NapCatQQ (Onebot v11, go-cqhttp, NapCat protocols)"
@@ -21,8 +21,8 @@ dependencies {
     api("org.jetbrains:annotations:26.0.2")
     api("org.greenrobot:eventbus-java:3.3.1")
     api("org.slf4j:slf4j-api:2.0.17")
-    api("com.fasterxml.jackson.core:jackson-databind:2.19.2")
-    api("org.java-websocket:Java-WebSocket:1.6.0")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.19.2")
+    implementation("org.java-websocket:Java-WebSocket:1.6.0")
 
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
