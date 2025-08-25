d80057bf69f40017cae89afd71ec2112e1fd0b44
while1cry <while1cry@163.com>
2025-08-25 13:30:02 +0800

    chore: 删除无效导入

--- src/main/java/me/while1cry/napcat4j/entity/user/User.java
+++ src/main/java/me/while1cry/napcat4j/entity/user/User.java
@@ -1,10 +1,4 @@
 package me.while1cry.napcat4j.entity.user;
 
-import me.while1cry.napcat4j.entity.message.Message;
-
-import java.util.concurrent.CompletableFuture;
-
 public class User {
-
-//    public CompletableFuture<String> sendMessage(Message message) {}
 }
