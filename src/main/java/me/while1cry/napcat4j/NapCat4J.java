53fe31576577963b185374b2a971efb59b220ed4
while1cry <while1cry@163.com>
2025-08-25 09:25:28 +0800

    feat: 新增NapCat4J类作为机器人的主要入口

--- src/main/java/me/while1cry/napcat4j/NapCat4J.java
+++ src/main/java/me/while1cry/napcat4j/NapCat4J.java
@@ -1,48 +1,68 @@
 package me.while1cry.napcat4j;
 
 import lombok.Getter;
-import lombok.SneakyThrows;
 import me.while1cry.napcat4j.client.BotClient;
-import me.while1cry.napcat4j.client.ClientType;
+import me.while1cry.napcat4j.client.BotType;
 import me.while1cry.napcat4j.client.WebSocketBotClient;
+import me.while1cry.napcat4j.event.EventManager;
 import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.Closeable;
+import java.net.URI;
 import java.time.Duration;
 
 public class NapCat4J implements Closeable {
 
+    @Getter
     private final Logger logger;
     @Getter
-    private final String address;
-    @Getter
-    private final ClientType clientType;
+    private final URI address;
     @Getter
     @Nullable
     private final String token;
     @Getter
+    private final Duration timeout;
+    @Getter
+    private final BotType botType;
+    @Getter
     private final BotClient botClient;
+    @Getter
+    private final EventManager eventManager;
 
-    @SneakyThrows
-    public NapCat4J(String address, ClientType clientType, @Nullable String token, Duration timeout) {
+    public NapCat4J(URI address, BotType botType, @Nullable String token, Duration timeout) {
         this.logger = LoggerFactory.getLogger("NapCat4J");
         this.address = address;
-        this.clientType = clientType;
         this.token = token;
+        this.timeout = timeout;
+        this.botType = botType;
 
-        this.botClient = switch (clientType) {
-            case WEBSOCKET_CLIENT -> new WebSocketBotClient(logger, address, token, timeout);
+        this.eventManager = new EventManager(logger);
+        this.botClient = switch (botType) {
+            case WEBSOCKET_CLIENT -> new WebSocketBotClient(this);
+        };
+    }
+
+    public NapCat4J(Logger logger, URI address, BotType botType, @Nullable String token, Duration timeout) {
+        this.logger = logger;
+        this.address = address;
+        this.token = token;
+        this.timeout = timeout;
+        this.botType = botType;
+
+        this.eventManager = new EventManager(logger);
+        this.botClient = switch (botType) {
+            case WEBSOCKET_CLIENT -> new WebSocketBotClient(this);
         };
     }
 
     public void connect() {
-        botClient.createConnection();
+        botClient.connect();
     }
 
     @Override
     public void close() {
-        botClient.disconnect();
+        botClient.close();
     }
 }
