c22b7f6e2e94cf63437922874efe1429b8c271e6
while1cry <while1cry@163.com>
2025-08-25 09:23:47 +0800

    feat: 新增部分实体类

--- src/main/java/me/while1cry/napcat4j/entity/user/LoginInfo.java
+++ src/main/java/me/while1cry/napcat4j/entity/user/LoginInfo.java
@@ -1,4 +1,16 @@
 package me.while1cry.napcat4j.entity.user;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class LoginInfo {
+
+    @JsonProperty("user_id")
+    private final String userId;
+
+    @JsonProperty("nickname")
+    private final String nickname;
 }
