6bfa41a2e0ffc69af62cda13c32a8cd88c093fe3
while1cry <while1cry@163.com>
2025-08-25 09:20:43 +0800

    refactor: 提取Client可复用部分为AbstractClient

--- src/main/java/me/while1cry/napcat4j/client/BotClient.java
+++ src/main/java/me/while1cry/napcat4j/client/BotClient.java
@@ -1,12 +1,279 @@
 package me.while1cry.napcat4j.client;
 
-public interface BotClient extends OneBotAPI, GoCQHTTPAPI, NapCatAPI {
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import lombok.AllArgsConstructor;
+import lombok.SneakyThrows;
+import me.while1cry.napcat4j.NapCat4J;
+import me.while1cry.napcat4j.dto.group.GroupDto;
+import me.while1cry.napcat4j.dto.group.GroupMemberDto;
+import me.while1cry.napcat4j.dto.user.FriendDto;
+import me.while1cry.napcat4j.dto.user.UserDto;
+import me.while1cry.napcat4j.entity.message.Message;
+import me.while1cry.napcat4j.entity.status.OnlineStatus;
+import me.while1cry.napcat4j.entity.user.LoginInfo;
+import me.while1cry.napcat4j.event.EventManager;
+import me.while1cry.napcat4j.event.message.GroupMessageEvent;
+import me.while1cry.napcat4j.event.message.PrivateMessageEvent;
+import me.while1cry.napcat4j.event.meta.ConnectMetaEvent;
+import me.while1cry.napcat4j.event.meta.HeartbeatMetaEvent;
+import org.jetbrains.annotations.Nullable;
+import org.slf4j.Logger;
 
-    void createConnection();
+import java.io.Closeable;
+import java.net.URI;
+import java.time.Duration;
+import java.util.Set;
+import java.util.concurrent.CompletableFuture;
 
-    boolean isConnected();
+@AllArgsConstructor
+public abstract class BotClient implements OneBotAPI, GoCQHTTPAPI, NapCatAPI, Closeable {
 
-    void disconnect();
+    protected static final ObjectMapper mapper = new ObjectMapper();
 
-    void close();
+    protected final NapCat4J core;
+    protected final Logger logger = core.getLogger();
+    protected final URI uri = core.getAddress();
+    protected final EventManager eventManager = core.getEventManager();
+
+    public abstract void connect();
+
+    public abstract boolean isConnected();
+
+    public abstract void close();
+
+    @Override
+    public CompletableFuture<Boolean> deleteFriend(String userId, boolean blacklist, boolean bothDel) {
+        ObjectNode params = mapper.createObjectNode()
+                .put("user_id", userId)
+                .put("temp_block", blacklist)
+                .put("temp_both_del", bothDel);
+        return send("delete_friend", params)
+                .thenApply(rsp -> rsp.get("data").get("result").asInt() == 0);
+    }
+
+    @Override
+    public CompletableFuture<Boolean> setGroupSign(String groupId) {
+        ObjectNode params = mapper.createObjectNode()
+                .put("group_id", groupId);
+        return send("set_group_sign", params)
+                .thenApply(rsp -> rsp.get("status").asText().equals("ok"));
+    }
+
+    @Override
+    public CompletableFuture<Boolean> setOnlineStatus(OnlineStatus status) {
+        ObjectNode params = mapper.valueToTree(status);
+        return send("set_online_status", params)
+                .thenApply(rsp -> rsp.get("status").asText().equals("ok"));
+    }
+
+    @Override
+    @SneakyThrows
+    public CompletableFuture<String> sendPrivateMessage(String userId, Message message) {
+        ObjectNode params = mapper.createObjectNode()
+                .put("user_id", userId)
+                .set("message", mapper.readTree(message.toJson()));
+        return send("send_private_msg", params)
+                .thenApply(rsp -> rsp.get("data").get("message_id").asText());
+    }
+
+    @Override
+    @SneakyThrows
+    public CompletableFuture<String> sendGroupMessage(String groupId, Message message) {
+        ObjectNode params = mapper.createObjectNode()
+                .put("group_id", groupId)
+                .set("message", mapper.readTree(message.toJson()));
+        return send("send_group_msg", params)
+                .thenApply(rsp -> rsp.get("data").get("message_id").asText());
+    }
+
+    @Override
+    public CompletableFuture<Boolean> recallMessage(String messageId) {
+        ObjectNode params = mapper.createObjectNode().put("message_id", messageId);
+        return send("delete_msg", params)
+                .thenApply(rsp -> rsp.get("status").asText().equals("ok"));
+    }
+
+    @Override
+    public CompletableFuture<Message> getMessage(String messageId) {
+        ObjectNode params = mapper.createObjectNode().put("message_id", messageId);
+        return send("get_msg", params)
+                .thenApply(rsp -> Message.fromJson((ArrayNode) rsp.get("data").get("message")));
+    }
+
+    @Override
+    public CompletableFuture<Boolean> sendLike(String userId, int times) {
+        ObjectNode params = mapper.createObjectNode()
+                .put("user_id", userId)
+                .put("times", times);
+        return send("send_like", params)
+                .thenApply(rsp -> rsp.get("status").asText().equals("ok"));
+    }
+
+    @Override
+    public CompletableFuture<Boolean> kickGroupMember(String groupId, String userId, boolean blacklist) {
+        ObjectNode params = mapper.createObjectNode()
+                .put("group_id", groupId)
+                .put("user_id", userId)
+                .put("reject_add_request", blacklist);
+        return send("set_group_kick", params)
+                .thenApply(rsp -> rsp.get("status").asText().equals("ok"));
+    }
+
+    @Override
+    public CompletableFuture<Boolean> banGroupMember(String groupId, String userId, Duration duration) {
+        ObjectNode params = mapper.createObjectNode()
+                .put("group_id", groupId)
+                .put("user_id", userId)
+                .put("duration", duration.toSeconds());
+        return send("set_group_ban", params)
+                .thenApply(rsp -> rsp.get("status").asText().equals("ok"));
+    }
+
+    @Override
+    public CompletableFuture<Boolean> banGroup(String groupId, boolean b) {
+        ObjectNode params = mapper.createObjectNode()
+                .put("group_id", groupId)
+                .put("enable", b);
+        return send("set_group_whole_ban", params)
+                .thenApply(rsp -> rsp.get("status").asText().equals("ok"));
+    }
+
+    @Override
+    public CompletableFuture<Boolean> setGroupAdmin(String groupId, String userId, boolean b) {
+        ObjectNode params = mapper.createObjectNode()
+                .put("group_id", groupId)
+                .put("user_id", userId)
+                .put("enable", b);
+        return send("set_group_whole_ban", params)
+                .thenApply(rsp -> rsp.get("status").asText().equals("ok"));
+    }
+
+    @Override
+    public CompletableFuture<Boolean> leaveGroup(String groupId) {
+        ObjectNode params = mapper.createObjectNode()
+                .put("group_id", groupId);
+        return send("set_group_leave", params)
+                .thenApply(rsp -> rsp.get("status").asText().equals("ok"));
+    }
+
+    @Override
+    public CompletableFuture<Boolean> setFriendAddRequest(String flag, boolean approve, String remark) {
+        ObjectNode params = mapper.createObjectNode()
+                .put("flag", flag)
+                .put("approve", approve)
+                .put("remark", remark);
+        return send("set_friend_add_request", params)
+                .thenApply(rsp -> rsp.get("status").asText().equals("ok"));
+    }
+
+    @Override
+    public CompletableFuture<Boolean> setGroupAddRequest(String flag, boolean approve, @Nullable String denyReason) {
+        ObjectNode params = mapper.createObjectNode()
+                .put("flag", flag)
+                .put("approve", approve);
+        if (denyReason != null)
+            params.put("reason", denyReason);
+        return send("set_group_add_request", params)
+                .thenApply(rsp -> rsp.get("status").asText().equals("ok"));
+    }
+
+    @Override
+    public CompletableFuture<LoginInfo> getLoginInfo() {
+        return send("get_login_info", mapper.createObjectNode())
+                .thenApply(rsp -> new LoginInfo(rsp.get("data").get("user_id").asText(), rsp.get("data").get("nickname").asText()));
+    }
+
+    @Override
+    public CompletableFuture<UserDto> getStrangerInfo(String userId) {
+        return null;
+    }
+
+    @Override
+    public CompletableFuture<Set<FriendDto>> getFriendList(boolean noCache) {
+        return null;
+    }
+
+    @Override
+    public CompletableFuture<GroupDto> getGroupInfo(String groupId) {
+        return null;
+    }
+
+    @Override
+    public CompletableFuture<Set<GroupDto>> getGroupList() {
+        return null;
+    }
+
+    @Override
+    public CompletableFuture<GroupMemberDto> getGroupMemberInfo(String groupId, String userId) {
+        return null;
+    }
+
+    @Override
+    public CompletableFuture<Set<GroupMemberDto>> getGroupMemberList(String groupId) {
+        return null;
+    }
+
+    @Override
+    public CompletableFuture<Boolean> cleanCache() {
+        return send("clean_cache", mapper.createObjectNode())
+                .thenApply(rsp -> rsp.get("status").asText().equals("ok"));
+    }
+
+    protected void eventHandle(String event) {
+        try {
+            ObjectNode json = (ObjectNode) mapper.readTree(event);
+            String type = json.get("post_type").asText();
+            switch (type) {
+                case "meta_event" -> {
+                    String metaEventType = json.get("meta_event_type").asText();
+                    @Nullable String subType = json.get("sub_type") == null ? null : json.get("sub_type").asText();
+                    switch (metaEventType) {
+                        case "lifecycle" -> {
+                            if (subType != null && subType.equals("connect")) {
+                                eventManager.callEvent(new ConnectMetaEvent(json.get("time").asLong(), json.get("self_id").asText()));
+                            }
+                        }
+                        case "heartbeat" -> eventManager.callEvent(new HeartbeatMetaEvent(json.get("time").asLong(),
+                                json.get("self_id").asText(),
+                                new HeartbeatMetaEvent.Status(
+                                        json.get("status").get("online").asBoolean(),
+                                        json.get("status").get("good").asBoolean()),
+                                json.get("interval").asLong()));
+                        default -> logger.warn("Unknown meta event type: {}", metaEventType);
+                    }
+                }
+                case "message" -> {
+                    Message message = Message.fromJson((ArrayNode) json.get("message"));
+                    switch (json.get("message_type").asText()) {
+                        case "group" -> eventManager.callEvent(new GroupMessageEvent(
+                                json.get("group_id").asText(),
+                                json.get("user_id").asText(),
+                                json.get("message_id").asText(),
+                                json.get("raw_message").asText(),
+                                message
+                        ));
+                        case "private" -> {
+                            eventManager.callEvent(new PrivateMessageEvent(
+                                    json.get("sub_type").asText(),
+                                    json.get("user_id").asText(),
+                                    json.get("message_id").asText(),
+                                    json.get("raw_message").asText(),
+                                    message
+                            ));
+                        }
+                    }
+                }
+                case "notice" -> {
+                    // TODO
+                }
+                default -> logger.warn("Unknown event type: {}", type);
+            }
+        } catch (Exception e) {
+            logger.error("Error parsing event", e);
+        }
+    }
+
+    protected abstract CompletableFuture<ObjectNode> send(String action, ObjectNode params);
 }
--- src/main/java/me/while1cry/napcat4j/client/WebSocketBotClient.java
+++ src/main/java/me/while1cry/napcat4j/client/WebSocketBotClient.java
@@ -3,310 +3,137 @@ package me.while1cry.napcat4j.client;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import lombok.Getter;
-import me.while1cry.napcat4j.dto.group.GroupDto;
-import me.while1cry.napcat4j.dto.group.GroupMemberDto;
-import me.while1cry.napcat4j.dto.user.FriendDto;
-import me.while1cry.napcat4j.dto.user.UserDto;
-import me.while1cry.napcat4j.entity.message.Message;
-import me.while1cry.napcat4j.entity.status.OnlineStatus;
-import me.while1cry.napcat4j.entity.user.LoginInfo;
+import me.while1cry.napcat4j.NapCat4J;
 import me.while1cry.napcat4j.event.EventCaller;
-import me.while1cry.napcat4j.event.EventManager;
-import me.while1cry.napcat4j.event.message.GroupMessageEvent;
-import me.while1cry.napcat4j.event.message.PrivateMessageEvent;
-import me.while1cry.napcat4j.event.meta.ConnectMetaEvent;
 import org.java_websocket.client.WebSocketClient;
-import org.java_websocket.enums.ReadyState;
 import org.java_websocket.handshake.ServerHandshake;
-import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
 import java.time.Duration;
-import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeUnit;
-import java.util.function.Function;
 
-public class WebSocketBotClient extends WebSocketClient implements BotClient, EventCaller {
+public class WebSocketBotClient extends BotClient implements EventCaller {
 
     private static final ObjectMapper mapper = new ObjectMapper();
-    private final String token;
+
     private final Logger logger;
-    private final Duration TIMEOUT;
-    private final ConcurrentHashMap<UUID, CompletableFuture<ObjectNode>> pending = new ConcurrentHashMap<>();
-    @Getter
-    private final EventManager eventManager;
+    private final String token;
+    private final Duration timeout;
+    private final Client client;
 
-    public WebSocketBotClient(String address, @Nullable String token) throws URISyntaxException {
-        super(new URI(address));
-        this.token = token;
-        this.logger = LoggerFactory.getLogger("NapCat4J");
-        this.TIMEOUT = Duration.ofSeconds(5);
-
-        this.eventManager = new EventManager(logger);
-    }
-
-    public WebSocketBotClient(Logger logger, String address, @Nullable String token, Duration timeout) throws URISyntaxException {
-        super(new URI(address));
-        this.token = token;
-        this.logger = logger;
-        this.TIMEOUT = timeout;
-
-        this.eventManager = new EventManager(logger);
+    public WebSocketBotClient(NapCat4J core) {
+        super(core);
+        this.logger = core.getLogger();
+        this.token = core.getToken();
+        this.timeout = core.getTimeout();
+        this.client = new Client(this);
     }
 
     @Override
-    public void createConnection() {
-        if (token != null)
-            addHeader("Authorization", "Bearer " + token);
-        logger.info("Connecting to {}...", getURI());
-        connect();
+    public void connect() {
+        client.connect();
     }
 
     @Override
     public boolean isConnected() {
-        return getReadyState().equals(ReadyState.OPEN);
+        return client.isOpen();
     }
 
     @Override
-    public void disconnect() {
-        this.close();
+    public void close() {
+        client.close();
     }
 
     @Override
-    public void onOpen(ServerHandshake serverHandshake) {
+    protected CompletableFuture<ObjectNode> send(String action, ObjectNode params) {
+        return client.sendWithEcho(action, params);
     }
 
-    @Override
-    public void onMessage(String s) {
-        try {
-            ObjectNode json = (ObjectNode) mapper.readTree(s);
-            if (json.has("echo")) {
-                UUID echo = UUID.fromString(json.get("echo").asText());
-                CompletableFuture<ObjectNode> future = pending.remove(echo);
-                if (future != null) {
-                    future.complete(json);
-                }
-            } else {
-                eventHandle(s);
-            }
-        } catch (Exception e) {
-            logger.error("Error parsing message", e);
+    private static final class Client extends WebSocketClient {
+
+        private final WebSocketBotClient bot;
+        private final Logger logger;
+        private final ConcurrentHashMap<UUID, CompletableFuture<ObjectNode>> pending = new ConcurrentHashMap<>();
+
+        private Client(WebSocketBotClient bot) {
+            super(bot.uri);
+            this.bot = bot;
+            this.logger = bot.logger;
         }
-    }
 
-    @Override
-    public void onMessage(ByteBuffer bytes) {
-        String message = StandardCharsets.UTF_8.decode(bytes).toString();
-        onMessage(message);
-    }
+        @Override
+        public void connect() {
+            if (bot.token != null)
+                addHeader("Authorization", "Bearer " + bot.token);
+            super.connect();
+        }
 
-    @Override
-    public void onClose(int i, String s, boolean b) {
-        logger.info("Disconnected from {}", getURI());
-    }
+        @Override
+        public void onOpen(ServerHandshake serverHandshake) {
+            logger.info("Connection opened.");
+        }
 
-    @Override
-    public void onError(Exception e) {
-        logger.error("Web socket client error", e);
-    }
-
-    private void eventHandle(String event) {
-        try {
-            ObjectNode json = (ObjectNode) mapper.readTree(event);
-            String type = json.get("post_type").asText();
-            switch (type) {
-                case "meta_event" -> {
-                    String metaEventType = json.get("meta_event_type").asText();
-                    @Nullable String subType = json.get("sub_type") == null ? null : json.get("sub_type").asText();
-                    switch (metaEventType) {
-                        case "lifecycle" -> {
-                            if (subType != null && subType.equals("connect")) {
-                                eventManager.callEvent(new ConnectMetaEvent(json.get("time").asLong(), json.get("self_id").asText()));
-                            }
-                        }
-                        case "heartbeat" -> {
-                            logger.debug("Receive heartbeat from {}", getURI());
-                        }
-                        default -> logger.warn("Unknown meta event type: {}", metaEventType);
+        @Override
+        public void onMessage(String s) {
+            try {
+                ObjectNode json = (ObjectNode) mapper.readTree(s);
+                if (json.has("echo")) {
+                    UUID echo = UUID.fromString(json.get("echo").asText());
+                    CompletableFuture<ObjectNode> future = pending.remove(echo);
+                    if (future != null) {
+                        future.complete(json);
                     }
+                } else {
+                    bot.eventHandle(s);
                 }
-                case "message" -> {
-                    ObjectNode node = mapper.createObjectNode();
-                    node.set("messages", json.get("message"));
-                    Message message = mapper.treeToValue(node, Message.class);
-                    switch (json.get("message_type").asText()) {
-                        case "group" -> eventManager.callEvent(new GroupMessageEvent(
-                                json.get("group_id").asText(),
-                                json.get("user_id").asText(),
-                                json.get("message_id").asText(),
-                                json.get("raw_message").asText(),
-                                message
-                        ));
-                        case "private" -> eventManager.callEvent(new PrivateMessageEvent(
-                                json.get("sub_type").asText(),
-                                json.get("user_id").asText(),
-                                json.get("message_id").asText(),
-                                json.get("raw_message").asText(),
-                                message
-                        ));
-                    }
-                }
-                case "notice" -> {
-                    // TODO
-                }
-                default -> logger.warn("Unknown event type: {}", type);
+            } catch (Exception e) {
+                logger.error("Error parsing message", e);
             }
-        } catch (Exception e) {
-            logger.error("Error parsing event", e);
-        }
-    }
-
-    @Override
-    public CompletableFuture<Boolean> deleteFriend(String userId, boolean blacklist, boolean bothDel) {
-        return null;
-    }
-
-    @Override
-    public CompletableFuture<Boolean> setGroupSign(String groupId) {
-        return null;
-    }
-
-    @Override
-    public CompletableFuture<Boolean> setOnlineStatus(OnlineStatus status) {
-        return null;
-    }
-
-    @Override
-    public CompletableFuture<String> sendPrivateMessage(String userId, Message message) {
-        return null;
-    }
-
-    @Override
-    public CompletableFuture<String> sendGroupMessage(String groupId, Message message) {
-        return null;
-    }
-
-    @Override
-    public CompletableFuture<Boolean> recallMessage(String messageId) {
-        return null;
-    }
-
-    @Override
-    public CompletableFuture<Message> getMessage(String messageId) {
-        return null;
-    }
-
-    @Override
-    public CompletableFuture<Boolean> sendLike(String userId, int times) {
-        return null;
-    }
-
-    @Override
-    public CompletableFuture<Boolean> kickGroupMember(String groupId, String userId, boolean blacklist) {
-        return null;
-    }
-
-    @Override
-    public CompletableFuture<Boolean> banGroupMember(String groupId, String userId, Duration duration) {
-        return null;
-    }
-
-    @Override
-    public CompletableFuture<Boolean> banGroup(String groupId, boolean b) {
-        return null;
-    }
-
-    @Override
-    public CompletableFuture<Boolean> setGroupAdmin(String groupId, String userId, boolean admin) {
-        return null;
-    }
-
-    @Override
-    public CompletableFuture<Boolean> leaveGroup(String groupId) {
-        return null;
-    }
-
-    @Override
-    public CompletableFuture<Boolean> setFriendAddRequest(String flag, boolean approve, String remark) {
-        return null;
-    }
-
-    @Override
-    public CompletableFuture<Boolean> setGroupAddRequest(String flag, boolean approve, String denyReason) {
-        return null;
-    }
-
-    @Override
-    public CompletableFuture<LoginInfo> getLoginInfo() {
-        return null;
-    }
-
-    @Override
-    public CompletableFuture<UserDto> getStrangerInfo(String userId) {
-        return null;
-    }
-
-    @Override
-    public CompletableFuture<Set<FriendDto>> getFriendList(boolean noCache) {
-        return null;
-    }
-
-    @Override
-    public CompletableFuture<GroupDto> getGroupInfo(String groupId) {
-        return null;
-    }
-
-    @Override
-    public CompletableFuture<Set<GroupDto>> getGroupList() {
-        return null;
-    }
-
-    @Override
-    public CompletableFuture<GroupMemberDto> getGroupMemberInfo(String groupId, String userId) {
-        return null;
-    }
-
-    @Override
-    public CompletableFuture<Set<GroupMemberDto>> getGroupMemberList(String groupId) {
-        return null;
-    }
-
-    @Override
-    public CompletableFuture<Boolean> cleanCache() {
-        return null;
-    }
-
-    private <T> CompletableFuture<T> send(
-            String action,
-            ObjectNode params,
-            Function<ObjectNode, T> resultExtractor
-    ) {
-        UUID echo = UUID.randomUUID();
-
-        ObjectNode body = mapper.createObjectNode()
-                .put("action", action)
-                .put("echo", echo.toString())
-                .set("params", params);
-
-        CompletableFuture<ObjectNode> futureResponse = new CompletableFuture<>();
-        pending.put(echo, futureResponse);
-
-        try {
-            send(mapper.writeValueAsString(body));
-        } catch (JsonProcessingException e) {
-            logger.error("Error sending action {}", action, e);
-            futureResponse.completeExceptionally(e);
         }
 
-        return futureResponse.thenApply(resultExtractor).orTimeout(TIMEOUT.toMillis(), TimeUnit.MILLISECONDS);
+        @Override
+        public void onMessage(ByteBuffer bytes) {
+            String message = StandardCharsets.UTF_8.decode(bytes).toString();
+            onMessage(message);
+        }
+
+        @Override
+        public void onClose(int i, String s, boolean b) {
+            logger.info("Disconnected from {}", getURI());
+        }
+
+        @Override
+        public void onError(Exception e) {
+            logger.error("Web socket client error", e);
+        }
+
+        private CompletableFuture<ObjectNode> sendWithEcho(
+                String action,
+                ObjectNode params
+        ) {
+            UUID echo = UUID.randomUUID();
+
+            ObjectNode body = mapper.createObjectNode()
+                    .put("action", action)
+                    .put("echo", echo.toString())
+                    .set("params", params);
+
+            CompletableFuture<ObjectNode> futureResponse = new CompletableFuture<>();
+            pending.put(echo, futureResponse);
+
+            try {
+                send(mapper.writeValueAsString(body));
+            } catch (JsonProcessingException e) {
+                logger.error("Error sending action {}", action, e);
+                futureResponse.completeExceptionally(e);
+            }
+
+            return futureResponse.orTimeout(bot.timeout.toMillis(), TimeUnit.MILLISECONDS);
+        }
     }
 }
