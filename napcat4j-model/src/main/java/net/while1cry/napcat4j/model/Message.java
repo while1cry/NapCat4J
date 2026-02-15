9373981214828483bab3c7ae272ab2a3a2facb2b
while1cry <while1cry@163.com>
2026-02-15 22:01:43 +0800

    feat: 支持部分消息格式(video, rps, dice, contact, reply, json, mface, file, unknown)

--- napcat4j-model/src/main/java/net/while1cry/napcat4j/model/Message.java
+++ napcat4j-model/src/main/java/net/while1cry/napcat4j/model/Message.java
@@ -243,6 +243,59 @@ public class Message {
         }
     }
 
+    private static class VideoMessageData extends MessageData {
+        @NotNull
+        private final String file;
+        @Nullable
+        private final String name;
+        @Nullable
+        private final String thumb;
+        @Nullable
+        private final String url;
+        @Nullable
+        private final String path;
+        @Nullable
+        private final String fileId;
+        @Nullable
+        private final String fileSize;
+        @Nullable
+        private final String fileUnique;
+
+        private VideoMessageData(@NotNull String file,
+                                  @Nullable String name,
+                                  @Nullable String thumb,
+                                  @Nullable String url,
+                                  @Nullable String path,
+                                  @Nullable String fileId,
+                                  @Nullable String fileSize,
+                                  @Nullable String fileUnique) {
+            super("video");
+            this.file = file;
+            this.name = name;
+            this.thumb = thumb;
+            this.url = url;
+            this.path = path;
+            this.fileId = fileId;
+            this.fileSize = fileSize;
+            this.fileUnique = fileUnique;
+        }
+
+        @Override
+        protected JsonNode toJson() {
+            return mapper.createObjectNode()
+                    .put("type", type)
+                    .set("data", mapper.createObjectNode()
+                            .put("file", file)
+                            .put("name", name)
+                            .put("thumb", thumb)
+                            .put("url", url)
+                            .put("path", path)
+                            .put("file_id", fileId)
+                            .put("file_size", fileSize)
+                            .put("file_unique", fileUnique));
+        }
+    }
+
     private static class AtMessageData extends MessageData {
         @NotNull
         private final String qq;
@@ -258,6 +311,211 @@ public class Message {
                     .put("type", type)
                     .set("data", mapper.createObjectNode()
                             .put("qq", qq));
+        }
+    }
+
+    private static class RPSMessageData extends MessageData {
+        @Nullable
+        private final String result;
+
+        private RPSMessageData(@Nullable String result) {
+            super("rps");
+            this.result = result;
+        }
+
+        @Override
+        protected JsonNode toJson() {
+            return mapper.createObjectNode()
+                    .put("type", type)
+                    .set("data", mapper.createObjectNode()
+                            .put("result", result));
+        }
+    }
+
+    private static class DiceMessageData extends MessageData {
+        @Nullable
+        private final String result;
+
+        private DiceMessageData(@Nullable String result) {
+            super("dice");
+            this.result = result;
+        }
+
+        @Override
+        protected JsonNode toJson() {
+            return mapper.createObjectNode()
+                    .put("type", type)
+                    .set("data", mapper.createObjectNode()
+                            .put("result", result));
+        }
+    }
+
+    private static class ContactMessageData extends MessageData {
+        @Nullable
+        private final String id;
+
+        private ContactMessageData(@Nullable String id) {
+            super("qq");
+            this.id = id;
+        }
+
+        @Override
+        protected JsonNode toJson() {
+            return mapper.createObjectNode()
+                    .put("type", type)
+                    .set("data", mapper.createObjectNode()
+                            .put("id", id));
+        }
+    }
+
+    private static class ContactGroupMessageData extends MessageData {
+        @Nullable
+        private final String id;
+
+        private ContactGroupMessageData(@Nullable String id) {
+            super("group");
+            this.id = id;
+        }
+
+        @Override
+        protected JsonNode toJson() {
+            return mapper.createObjectNode()
+                    .put("type", type)
+                    .set("data", mapper.createObjectNode()
+                            .put("id", id));
+        }
+    }
+
+    private static class ReplyMessageData extends MessageData {
+        @NotNull
+        private final String id;
+
+        private ReplyMessageData(@NotNull String id) {
+            super("reply");
+            this.id = id;
+        }
+
+        @Override
+        protected JsonNode toJson() {
+            return mapper.createObjectNode()
+                    .put("type", type)
+                    .set("data", mapper.createObjectNode()
+                            .put("id", id));
+        }
+    }
+
+    private static class JsonMessageData extends MessageData {
+        @NotNull
+        private final String json;
+
+        private JsonMessageData(@NotNull String json) {
+            super("json");
+            this.json = json;
+        }
+
+        @Override
+        protected JsonNode toJson() {
+            return mapper.createObjectNode()
+                    .put("type", type)
+                    .set("data", mapper.createObjectNode()
+                            .set("data", mapper.readTree(json)));
+        }
+    }
+
+    private static class MFaceMessageData extends MessageData {
+        @Nullable
+        private final String emojiId;
+        @Nullable
+        private final String emojiPackageId;
+        @Nullable
+        private final String key;
+        @Nullable
+        private final String summary;
+
+        private MFaceMessageData(@Nullable String emojiId,
+                                 @Nullable String emojiPackageId,
+                                 @Nullable String key,
+                                 @Nullable String summary) {
+            super("mface");
+            this.emojiId = emojiId;
+            this.emojiPackageId = emojiPackageId;
+            this.key = key;
+            this.summary = summary;
+        }
+
+        @Override
+        protected JsonNode toJson() {
+            return mapper.createObjectNode()
+                    .put("type", type)
+                    .set("data", mapper.createObjectNode()
+                            .put("emoji_id", emojiId)
+                            .put("emoji_package_id", emojiPackageId)
+                            .put("key", key)
+                            .put("summary", summary));
+        }
+    }
+
+    private static class FileMessageData extends MessageData {
+        @Nullable
+        private final String name;
+        @NotNull
+        private final String file;
+        @Nullable
+        private final String path;
+        @Nullable
+        private final String url;
+        @Nullable
+        private final String fileId;
+        @Nullable
+        private final String fileUnique;
+
+        private FileMessageData(@Nullable String name,
+                                @NotNull String file,
+                                @Nullable String path,
+                                @Nullable String url,
+                                @Nullable String fileId,
+                                @Nullable String fileUnique) {
+            super("file");
+            this.name = name;
+            this.file = file;
+            this.path = path;
+            this.url = url;
+            this.fileId = fileId;
+            this.fileUnique = fileUnique;
+        }
+
+        @Override
+        protected JsonNode toJson() {
+            return mapper.createObjectNode()
+                    .put("type", type)
+                    .set("data", mapper.createObjectNode()
+                            .put("name", name)
+                            .put("file", file)
+                            .put("path", path)
+                            .put("url", url)
+                            .put("file_id", fileId)
+                            .put("file_unique", fileUnique));
+        }
+    }
+
+    private static class UnknownMessageData extends MessageData {
+        @Nullable
+        private final String type;
+        @Nullable
+        private final String dataJson;
+
+        private UnknownMessageData(@Nullable String type,
+                                   @Nullable String dataJson) {
+            super(type);
+            this.type = type;
+            this.dataJson = dataJson;
+        }
+
+        @Override
+        protected JsonNode toJson() {
+            return mapper.createObjectNode()
+                    .put("type", type)
+                    .set("data", mapper.readTree(dataJson));
         }
     }
 
@@ -283,6 +541,43 @@ public class Message {
             return this;
         }
 
+        public MessageBuilder image(@Nullable String name,
+                                    @Nullable String summary,
+                                    @NotNull String file,
+                                    @Nullable String subType,
+                                    @Nullable String fileId,
+                                    @Nullable String url,
+                                    @Nullable String path,
+                                    @Nullable String fileSize,
+                                    @Nullable String fileUnique) {
+            messages.add(new ImageMessageData(name, summary, file, subType, fileId, url, path, fileSize, fileUnique));
+            return this;
+        }
+
+        public MessageBuilder record(@NotNull String file,
+                                     @Nullable String name,
+                                     @Nullable String thumb,
+                                     @Nullable String url,
+                                     @Nullable String path,
+                                     @Nullable String fileId,
+                                     @Nullable String fileSize,
+                                     @Nullable String fileUnique) {
+            messages.add(new RecordMessageData(file, name, thumb, url, path, fileId, fileSize, fileUnique));
+            return this;
+        }
+
+        public MessageBuilder video(@NotNull String file,
+                                    @Nullable String name,
+                                    @Nullable String thumb,
+                                    @Nullable String url,
+                                    @Nullable String path,
+                                    @Nullable String fileId,
+                                    @Nullable String fileSize,
+                                    @Nullable String fileUnique) {
+            messages.add(new VideoMessageData(file, name, thumb, url, path, fileId, fileSize, fileUnique));
+            return this;
+        }
+
         public MessageBuilder at(String qq) {
             messages.add(new AtMessageData(qq));
             return this;
@@ -290,6 +585,51 @@ public class Message {
 
         public MessageBuilder atAll() {
             messages.add(new AtMessageData("all"));
+            return this;
+        }
+
+        public MessageBuilder rps() {
+            messages.add(new RPSMessageData(null));
+            return this;
+        }
+
+        public MessageBuilder dice() {
+            messages.add(new DiceMessageData(null));
+            return this;
+        }
+        public MessageBuilder contact(String qq) {
+            messages.add(new ContactMessageData(qq));
+            return this;
+        }
+        public MessageBuilder contactGroup(String group) {
+            messages.add(new ContactGroupMessageData(group));
+            return this;
+        }
+        public MessageBuilder reply(String id) {
+            messages.add(new ReplyMessageData(id));
+            return this;
+        }
+
+        public MessageBuilder json(String json) {
+            messages.add(new JsonMessageData(json));
+            return this;
+        }
+
+        public MessageBuilder mFace(@Nullable String emojiId,
+                                    @Nullable String emojiPackageId,
+                                    @Nullable String key,
+                                    @Nullable String summary) {
+            messages.add(new MFaceMessageData(emojiId, emojiPackageId, key, summary));
+            return this;
+        }
+
+        public MessageBuilder file(@Nullable String name,
+                                   @NotNull String file,
+                                   @Nullable String path,
+                                   @Nullable String url,
+                                   @Nullable String fileId,
+                                   @Nullable String fileUnique) {
+            messages.add(new FileMessageData(name, file, path, url, fileId, fileUnique));
             return this;
         }
 
